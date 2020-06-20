99f26d50cadaacfa9c3a92f692d03e61d90a4206
tkgstrator <nasawake.am@gmail.com>
2020-06-20 14:32:35 -0400

    Update LoginMethod.

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <v-container>
       <Top />
       <OAuth />
+      <Players />
     </v-container>
   </v-app>
 </template>
@@ -11,12 +12,14 @@
 // import Dilemma from './components/Dilemma'
 import Top from "./components/Top";
 import OAuth from "./components/OAuth";
+import Players from "./components/Players";
 
 export default {
   name: "App",
   components: {
     OAuth,
     Top,
+    Players,
   },
 };
 </script>
---
+++ src/components/Players.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="players">
+    <transition-group name="players" tag="div" class="list content">
+      <section
+        v-for="{ name, value } in players"
+        :key="value"
+        class="playerlist"
+      >
+        <div class="name">{{ name }}</div>
+      </section>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import { db } from "../plugins/firebase";
+
+export default {
+  name: "Players",
+  data() {
+    return {
+      user: {},
+      name: {},
+      players: [],
+    };
+  },
+  firestore() {
+    return {
+      players: db.collection("users"),
+    };
+  },
+};
+</script>
